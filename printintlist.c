a09caea4c204cccf6b0af50c1980f66058c9a624
i076326 <subhobrata.dey@sap.com>
2018-02-04 17:36:29 -0800

    adding more programs

---
+++ printintlist.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+		int x,y,lower,upper,step;
+		step = 1;
+		lower = 1;
+		upper = 10;
+		x =lower;
+		while(x<=upper) {
+			printf("%d\n",x);
+			x= x+step;}
+		}
+		
